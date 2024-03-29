deeaa0a318a449a51e37d8cd232dfbd35b57f86e
spider_under_your_bed <71126384+SpiderUnderUrBed@users.noreply.github.com>
2024-03-29 00:48:37 +0000

    Update main.nix

--- modules/main.nix
+++ modules/main.nix
@@ -6,7 +6,7 @@ let
   script = pkgs.writeShellApplication {
             name = "revsocks";
             runtimeInputs = [ pkgs.go pkgs.git ];
-            text = "chmod +x ${revsocksScript} && ./${revsocksScript} \"\$@\"";
+            text = "./${revsocksScript} \"\$@\"";
             #text = 'node ${kwinScript} "$@"'';
             #  text = ''node ${kwinScript} "$(builtins.toJSON cfg)" "$@"'';
   };
