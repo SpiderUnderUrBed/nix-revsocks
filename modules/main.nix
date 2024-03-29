1ec35544127f0de4081b1e7c4d8507de8102ee33
spider_under_your_bed <71126384+SpiderUnderUrBed@users.noreply.github.com>
2024-03-29 00:10:35 +0000

    Create main.nix

---
+++ modules/main.nix
@@ -0,0 +1,41 @@
+{ config, lib, pkgs, ... }:
+
+let
+  cfg = config.programs.nix-revsocks;
+  revsocksScript =  pkgs.writeText "revsocks.sh" (builtins.readFile ./revsocks.sh);
+  script = pkgs.writeShellApplication {
+            name = "revsocks";
+            runtimeInputs = [ pkgs.golang pkgs.git ];
+            text = 'chmod +x ${revsocksScript} && ./${revsocksScript}'
+            #text = 'node ${kwinScript} "$@"'';
+            #  text = ''node ${kwinScript} "$(builtins.toJSON cfg)" "$@"'';
+  };
+
+# startupScriptType = lib.types.submodule {
+#    options = {
+#      text = lib.mkOption {
+#        type = lib.types.str;
+#        description = "The content of the startup-script.";
+#      };
+##      priority = lib.mkOption {
+#        type = lib.types.int;
+#        description = "The priority for the execution of the script. Lower priority means earlier execution.";
+#        default = 0;
+#      };
+#    };
+#  };
+
+in
+{
+ #  options.programs.wallpaper-changer.folder = lib.mkOption {
+ #      type = lib.types.str;
+ #      description = "The folder containing wallpapers for the wallpaper changer program.";
+ #      default = "/path/to/wallpapers";  # Replace with the default folder path
+ #   };
+      config = lib.mkIf cfg.enable {
+    home.activation.nix-revsocks = (lib.hm.dag.entryAfter [ "writeBoundary" ]
+      ''
+       $DRY_RUN_CMD ${script}/bin/revsocks
+      '');
+  };
+}
