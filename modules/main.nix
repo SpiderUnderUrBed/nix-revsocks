1c4cad90a9afc4d4f888e49be13d3e9c5c533221
spider_under_your_bed <71126384+SpiderUnderUrBed@users.noreply.github.com>
2024-03-29 23:23:17 +0000

    Update main.nix

--- modules/main.nix
+++ modules/main.nix
@@ -1,32 +1,5 @@
 { config, lib, pkgs, ... }:
 
-#let
-#  cfg = config.programs.nix-revsocks;
-#  revsocksSrc = pkgs.fetchFromGitHub {
-#    owner = "example";
-#    repo = "revsocks";  # Replace with the actual repository information
-#    rev = "master";  # Replace with the desired revision
-#    sha256 = "<SHA256 hash>";  # Replace with the actual hash
-#  };
-#  revsocksDir = pkgs.writeText "revsocksDir.nix" ''
-#    mkdir -p $out
-#    cp -r ${revsocksSrc}/* $out
-#  '';
-#  script = pkgs.writeShellScriptBin "revsocks-build" ''
-#    cd ${revsocksDir}
-#    make
-#  '';
-#in
-#{
-#  config = lib.mkIf cfg.enable {
-#    home.activation.nix-revsocks = (lib.hm.dag.entryAfter [ "writeBoundary" ]
-#      ''
-#      ${script}/bin/revsocks-build
-#      '');
-#  };
-#}
-
-#{ config, lib, pkgs, ... }:
 with import <nixpkgs> {};
 let
   cfg = config.programs.nix-revsocks;
@@ -52,36 +25,10 @@ let
               pkgs.gcc 
               pkgs.git
             ];
-            #\"\$@\"
-           #  text = "git clone https://github.com/kost/revsocks.git ${builtins.toString ./.}/revsocks && make -c ${builtins.toString ./.}/revsocks";
              text = "ls ${revsocksDerivation}";
-           #   text = "echo ${builtins.toString ../.} && cd .config/home-manager/result/home-files/.local/share/ && ls && git clone https://github.com/kost/revsocks.git && cd revsocks && make \"\$@\"";
-            # text = "./${revsocksScript} \"\$@\"";
-            #text = 'node ${kwinScript} "$@"'';
-            #  text = ''node ${kwinScript} "$(builtins.toJSON cfg)" "$@"'';
   };
-
-# startupScriptType = lib.types.submodule {
-#    options = {
-#      text = lib.mkOption {
-#        type = lib.types.str;
-#        description = "The content of the startup-script.";
-#      };
-##      priority = lib.mkOption {
-#        type = lib.types.int;
-#        description = "The priority for the execution of the script. Lower priority means earlier execution.";
-#        default = 0;
-#      };
-#    };
-#  };
-
 in
 {
- #  options.programs.wallpaper-changer.folder = lib.mkOption {
- #      type = lib.types.str;
-#      description = "The folder containing wallpapers for the wallpaper changer program.";
- #     default = "/path/to/wallpapers";  # Replace with the default folder path
- #  };
      config = lib.mkIf cfg.enable {
     home.activation.nix-revsocks = (lib.hm.dag.entryAfter [ "writeBoundary" ]
       ''
