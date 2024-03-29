486da1ffeecb61b7ff4f7602b1f10f32f080d251
spider_under_your_bed <71126384+SpiderUnderUrBed@users.noreply.github.com>
2024-03-29 07:35:32 +0000

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
 
-    revsocks.url = "github:kost/revsocks";
+    #revsocks.url = "github:kost/revsocks";
 
     home-manager.url = "github:nix-community/home-manager/release-23.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
