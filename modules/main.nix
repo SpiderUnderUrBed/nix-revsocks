eabe90691831ef060664732fea00008ff8513774
spider_under_your_bed <71126384+SpiderUnderUrBed@users.noreply.github.com>
2024-03-29 22:59:50 +0000

    Update main.nix

--- modules/main.nix
+++ modules/main.nix
@@ -31,6 +31,20 @@
 let
   cfg = config.programs.nix-revsocks;
  # revsocksScript =  pkgs.writeText "revsocks.sh" (builtins.readFile ./revsocks.sh);
+  revsocksDerivation = stdenv.mkDerivation {
+    name = "revsocks-src";
+    src = fetchgit {
+      url = "https://github.com/kost/revsocks.git";  # Replace with the actual GitHub repository URL
+      rev = "main";  # Replace with the desired revision
+      sha256 = "<SHA256 hash>";  # Replace with the actual hash
+    };
+    buildInputs = [ pkgs.make ];  # Add build inputs as needed
+    installPhase = ''
+      mkdir -p $out
+      cp -r $src/* $out
+      make -C $out  # Run make within the source directory
+    '';
+  };
   script = pkgs.writeShellApplication {
             name = "revsocks";
             runtimeInputs = [ 
