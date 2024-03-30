36946f1a1b7d789a34351dd464c4656706091240
spider_under_your_bed <71126384+SpiderUnderUrBed@users.noreply.github.com>
2024-03-30 20:14:23 +0000

    Update process.js

--- modules/process.js
+++ modules/process.js
@@ -5,13 +5,21 @@ const args = process.argv.slice(2);
 
 // Check if any arguments are provided
 if (args.length > 0) {
-  console.log("Parameters received:");
+//  console.log("Parameters received:");
   // Iterate over the arguments and print each one
-  args.forEach((param, index) => {
-    console.log(`${index + 1}. ${param}`);
-  });
-} else {
-  console.log("No parameters received.");
-}
+ // args.forEach((param, index) => {
+   // console.log(`${index + 1}. ${param}`);
+  //});
 
-// Your main script logic goes here
+  // Define variables based on args
+  let config, BIN;
+  try {
+    // Try to parse the arguments as JSON
+    config = JSON.parse(args.join(' '));
+   // console.log("Config:", config);
+  } catch (error) {
+    // If parsing fails, set BIN variable
+    BIN = args.join(' ');
+   // console.log("BIN:", BIN);
+  }
+}
