0ace6b5a2f87bd18fd7a5cb5a3742a831ffb3116
spider_under_your_bed <71126384+SpiderUnderUrBed@users.noreply.github.com>
2024-03-30 03:19:48 +0000

    Update main.nix

--- modules/main.nix
+++ modules/main.nix
@@ -9,7 +9,7 @@ let
     src = fetchgit {
       url = "https://github.com/kost/revsocks.git";  # Replace with the actual GitHub repository URL
       rev = "master";  # Replace with the desired revision
-      sha256 = "0avqxi0rhly2iwk9fj4dpniipckvykv8xixiak0kzaa853mlyd17";  # Replace with the actual hash
+      sha256 = "160a4fq5fa4i0l3plcx9w8679rpm4f5y6n00m95lsgw7l9c5qvzm";  # Replace with the actual hash
     };
     buildInputs = [ pkgs.cmake ];  # Add build inputs as needed
     installPhase = ''
