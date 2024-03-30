b6ca367675983e0e9b519856bec6b38487483beb
spider_under_your_bed <71126384+SpiderUnderUrBed@users.noreply.github.com>
2024-03-30 19:39:41 +0000

    Update process.js

--- modules/process.js
+++ modules/process.js
@@ -5,9 +5,11 @@ const args = process.argv.slice(2);
 
 // Check if any arguments are provided
 if (args.length > 0) {
-  // Assuming the first argument is the parameter you want to receive
-  const parameter = args[0];
-  console.log("Parameter received:", parameter);
+  console.log("Parameters received:");
+  // Iterate over the arguments and print each one
+  args.forEach((param, index) => {
+    console.log(`${index + 1}. ${param}`);
+  });
 } else {
   console.log("No parameters received.");
 }
