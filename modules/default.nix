60eb00329b57043bafe7c4e5f23d334a14a05242
spider_under_your_bed <71126384+SpiderUnderUrBed@users.noreply.github.com>
2024-03-30 22:34:36 +0000

    Update default.nix

--- modules/default.nix
+++ modules/default.nix
@@ -1,42 +1,15 @@
+User
 { lib, ... }:
-
-let
-  mapAttrs = lib.mapAttrs;
-
-  # Define the allowed options for instances
-  allowedInstanceOptions = {
-    option1 = "";
-    option2 = "";
-    # Add more allowed options as needed
-  };
-
-  # Function to validate instances
-  validateInstance = instanceName:
-    if lib.hasAttr instanceName allowedInstanceOptions
-    then null
-    else throw "${instanceName} is not a valid instance";
-in
 {
-  imports = [
-    ./main.nix
-  ];
+imports = [
+./main.nix
+];
+options.programs.nix-revsocks = { 
+enable = lib.mkEnableOption ''
+    Whether or not to enable revsocks
+  '';
+instances = [
 
-  options.programs.nix-revsocks = {
-    enable = lib.mkEnableOption ''
-      Whether or not to enable revsocks
-    '';
-    instances = lib.mkOption {
-      default = {};
-      type = lib.types.attrsOf lib.types.string;
-      description = ''
-        Instances configuration for nix-revsocks
-      '';
-      # Validate instances
-      value = mapAttrs (instanceName: _options:
-        if lib.hasAttr instanceName allowedInstanceOptions
-        then _options
-        else throw "${instanceName} is not a valid instance"
-      );
-    };
-  };
+];
+};
 }
