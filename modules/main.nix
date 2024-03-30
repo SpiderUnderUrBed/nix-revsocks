3a589edf7847255e9bbceada0fe85169d5a9f3d9
spider_under_your_bed <71126384+SpiderUnderUrBed@users.noreply.github.com>
2024-03-30 04:30:18 +0000

    Update main.nix

--- modules/main.nix
+++ modules/main.nix
@@ -1,5 +1,26 @@
 { config, lib, pkgs, ... }:
 
+#pet = buildGoModule rec {
+#  pname = "pet";
+#  version = "0.3.4";
+
+#  src = fetchFromGitHub {
+#    owner = "knqyf263";
+#    repo = "pet";
+#    rev = "v${version}";
+#    hash = "sha256-Gjw1dRrgM8D3G7v6WIM2+50r4HmTXvx0Xxme2fH9TlQ=";
+ # };
+
+#  vendorHash = "sha256-ciBIR+a1oaYH+H1PcC8cD8ncfJczk1IiJ8iYNM+R6aA=";
+
+ # meta = with lib; {
+##    description = "Simple command-line snippet manager, written in Go";
+ #   homepage = "https://github.com/knqyf263/pet";
+ #   license = licenses.mit;
+#    maintainers = with maintainers; [ kalbasit ];
+#  };
+#}
+
 with import <nixpkgs> {};
 let
   cfg = config.programs.nix-revsocks;
