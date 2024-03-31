8ec991598e33b5036c21d0711f6df03d6fdb5f92
spider_under_your_bed <71126384+SpiderUnderUrBed@users.noreply.github.com>
2024-03-31 15:32:23 +1300

    Update default.nix

--- modules/default.nix
+++ modules/default.nix
@@ -1,14 +1,43 @@
-{ lib, ... }:
-{
-imports = [
-./main.nix
-];
-options.programs.nix-revsocks = { 
-enable = lib.mkEnableOption ''
-    Whether or not to enable revsocks
-  '';
-instances = [
+{ lib, types, ... }:
 
-];
-};
+{
+  imports = [
+    ./main.nix
+  ];
+
+  options.programs.nix-revsocks = { 
+    enable = lib.mkEnableOption ''
+      Whether or not to enable revsocks
+    '';
+    instances = lib.mkOption {
+      description = "the instances of revsocks to start";
+      default = {};
+      type = types.attrsOf (
+        types.submodule ({ lib, types, ... }: {
+          options = {
+            password = types.mkOption {
+              description = "the password.";
+              type = types.str;
+            };
+            proxy = types.mkOption {
+              description = "the proxy it may use.";
+              type = types.str;
+            };
+            socks = types.mkOption {
+              description = "the place to host the socks proxy.";
+              type = types.str;
+            };
+            listen = types.mkOption {
+              description = "the port to listen too.";
+              type = types.str;
+            };
+            connect = types.mkOption {
+              description = "the connecting ip and port.";
+              type = types.str;
+            };
+          };
+        })
+      );
+    };
+  };
 }
