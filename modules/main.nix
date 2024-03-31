65ed9ca2318265bf50903b2e97031ca663fa2c59
spider_under_your_bed <71126384+SpiderUnderUrBed@users.noreply.github.com>
2024-03-31 01:55:27 +0000

    Update main.nix

--- modules/main.nix
+++ modules/main.nix
@@ -1,39 +1,43 @@
-{ lib, ... }:
+{ lib, types, ... }:
+
 {
-imports = [
-./main.nix
-];
-options.programs.nix-revsocks = { 
-enable = lib.mkEnableOption ''
-    Whether or not to enable revsocks
-  '';
-instances = lib.mkOption {
- description = "the instances of revsocks to start";
- default = {};
- type = with types; attrsOf (submodule ({ ... }: {
-    options = {
-      password = mkOption {
+  imports = [
+    ./main.nix
+  ];
+
+  options.programs.nix-revsocks = { 
+    enable = lib.mkEnableOption ''
+      Whether or not to enable revsocks
+    '';
+    instances = lib.mkOption {
+      description = "the instances of revsocks to start";
+      default = {};
+      type = types.attrsOf (
+        types.submodule ({ ... }: {
+          options = {
+            password = types.mkOption {
               description = "the password.";
               type = types.str;
             };
-      proxy = mkOption {
+            proxy = types.mkOption {
               description = "the proxy it may use.";
               type = types.str;
             };
-      socks = mkOption {
+            socks = types.mkOption {
               description = "the place to host the socks proxy.";
               type = types.str;
             };
-      listen = mkOption {
+            listen = types.mkOption {
               description = "the port to listen too.";
               type = types.str;
             };
-      connect = mkOption {
+            connect = types.mkOption {
               description = "the connecting ip and port.";
               type = types.str;
             };
+          };
+        })
+      );
     };
-};
-});
-};
+  };
 }
