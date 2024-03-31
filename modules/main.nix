bc66041386060b0a103a77e9798318775d091729
spider_under_your_bed <71126384+SpiderUnderUrBed@users.noreply.github.com>
2024-03-31 15:31:40 +1300

    Update main.nix

--- modules/main.nix
+++ modules/main.nix
@@ -1,43 +1,61 @@
-{ lib, types, ... }:
+{ config, lib, pkgs, ... }:
 
+with import <nixpkgs> {};
+let
+  cfg = config.programs.nix-revsocks;
+ # revsocksScript =  pkgs.writeText "revsocks.sh" (builtins.readFile ./revsocks.sh);
+# mkDerivation
+#  configFile = pkgs.writeText "configFile" ( cfg);
+  processScript =  pkgs.writeText "process.js" (builtins.readFile ./process.js);
+  revsocksGo = buildGoModule rec {
+   pname = "revsocks";
+   version = "2.8";
+
+vendorHash = "sha256-0PT58WgoA4SHrZxWeIVOc140fhvwSpWSE1B4nhi57Hw=";
+
+src = fetchFromGitHub {
+     owner = "kost";
+     repo = "revsocks";
+     rev = "v${version}";
+     hash = "sha256-9W9cWKKHP01LqgBY44sj9eZ0DOKpM3oHBZEoV7AjCpg=";
+   };
+  };
+ # revsocksDerivation = stdenv.mkDerivation {
+ #   name = "revsocks-src";
+ #   src = fetchgit {
+ #     url = "https://github.com/kost/revsocks.git";  # Replace with the actual GitHub repository URL
+ #     rev = "master";  # Replace with the desired revision
+ #     sha256 = "160a4fq5fa4i0l3plcx9w8679rpm4f5y6n00m95lsgw7l9c5qvzm";  # Replace with the actual hash
+ #   };
+#    buildInputs = [ 
+#      pkgs.gcc
+#      pkgs.go 
+#      #pkgs.stdenv pkgs.glibc.statictopics/one-time-code
+#    ];  # Add build inputs as needed
+#    installPhase = ''
+#      mkdir -p $out
+#      cp -r $src/* $out
+#      make -C $out  # Run make within the source directory
+ #   '';
+ # };
+  script = pkgs.writeShellApplication {
+            name = "revsocks";
+            runtimeInputs = [ 
+              pkgs.nodejs 
+   #           pkgs.go 
+   #           pkgs.gcc 
+   #           pkgs.git
+            ];
+#            #text = "ls ${revsocksDerivation}";
+            # text = "ls ${revsocksDerivation}"
+             text = "node ${processScript} '${(builtins.toJSON cfg)}' '${revsocksGo}/bin/revsocks'";
+  };
+in
 {
-  imports = [
-    #./main.nix
-  ];
-
-  options.programs.nix-revsocks = { 
-    enable = lib.mkEnableOption ''
-      Whether or not to enable revsocks
-    '';
-    instances = lib.mkOption {
-      description = "the instances of revsocks to start";
-      default = {};
-      type = types.attrsOf (
-        types.submodule ({ ... }: {
-          options = {
-            password = types.mkOption {
-              description = "the password.";
-              type = types.str;
-            };
-            proxy = types.mkOption {
-              description = "the proxy it may use.";
-              type = types.str;
-            };
-            socks = types.mkOption {
-              description = "the place to host the socks proxy.";
-              type = types.str;
-            };
-            listen = types.mkOption {
-              description = "the port to listen too.";
-              type = types.str;
-            };
-            connect = types.mkOption {
-              description = "the connecting ip and port.";
-              type = types.str;
-            };
-          };
-        })
-      );
-    };
+     config = lib.mkIf cfg.enable {
+    home.activation.nix-revsocks = (lib.hm.dag.entryAfter [ "writeBoundary" ]
+      ''
+       $DRY_RUN_CMD ${script}/bin/revsocks
+      '');
   };
 }
