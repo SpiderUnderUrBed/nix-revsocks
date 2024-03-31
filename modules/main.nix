e52f437b5974560fab02232a9d12c50e476fa503
spider_under_your_bed <71126384+SpiderUnderUrBed@users.noreply.github.com>
2024-03-31 01:45:05 +0000

    Update main.nix

--- modules/main.nix
+++ modules/main.nix
@@ -8,8 +8,32 @@ enable = lib.mkEnableOption ''
     Whether or not to enable revsocks
   '';
 instances = lib.mkOption {
-type = let inherit (lib) any isStringLike showOption;
-actualType = type.submodule ./instances-type.nix;
+ description = "the instances of revsocks to start";
+ default = {};
+ type = with types; attrsOf (submodule ({ ... }: {
+    options = {
+      password = mkOption {
+              description = "the password.";
+              type = types.str;
+            };
+      proxy = mkOption {
+              description = "the proxy it may use.";
+              type = types.str;
+            };
+      socks = mkOption {
+              description = "the place to host the socks proxy.";
+              type = types.str;
+            };
+      listen = mkOption {
+              description = "the port to listen too.";
+              type = types.str;
+            };
+      connect = mkOption {
+              description = "the connecting ip and port.";
+              type = types.str;
+            };
+    };
+})
 };
 };
 }
