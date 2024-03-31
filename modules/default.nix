8e9ff9d965556e744417b7cc1892a97cd726de82
spider_under_your_bed <71126384+SpiderUnderUrBed@users.noreply.github.com>
2024-03-31 15:25:46 +1300

    Update default.nix

--- modules/default.nix
+++ modules/default.nix
@@ -1,41 +1,14 @@
 { lib, ... }:
-
-let
-  instancesAllowedOptions = {
-    option1 = "";
-    option2 = "";
-    # Add more allowed options as needed
-  };
-
-  validateInstance = instanceName:
-    if lib.hasAttr instanceName instancesAllowedOptions
-    then null
-    else throw "${instanceName} is not a valid instance";
-
-  instances = {
-    # Define your instances here
-    instance1 = { option1 = ""; };
-    instance2 = { option2 = ""; };
-    # Add more instances as needed
-  };
-in
 {
-  imports = [
-    ./main.nix
-  ];
+imports = [
+./main.nix
+];
+options.programs.nix-revsocks = { 
+enable = lib.mkEnableOption ''
+    Whether or not to enable revsocks
+  '';
+instances = [
 
-  options.programs.nix-revsocks = {
-    enable = lib.mkEnableOption ''
-      Whether or not to enable revsocks
-    '';
-    instances = lib.mkIf (lib.isAttrs instances)
-      (lib.mapAttrs (instanceName: options:
-        if lib.hasAttr instanceName instancesAllowedOptions
-        then options
-        else throw "${instanceName} is not a valid instance"
-      ) instances)
-      null;
-  };
+];
+};
 }
-
-
