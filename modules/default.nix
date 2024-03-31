ae2df60025d04b0149398c08d6f313eae9bbffbb
spider_under_your_bed <71126384+SpiderUnderUrBed@users.noreply.github.com>
2024-03-31 03:12:36 +0000

    Update default.nix

--- modules/default.nix
+++ modules/default.nix
@@ -15,23 +15,23 @@
       type = with lib.types; attrsOf (
         types.submodule ({ lib, ... }: {
           options = {
-            password = types.mkOption {
+            password = lib.mkOption {
               description = "the password.";
               type = types.str;
             };
-            proxy = types.mkOption {
+            proxy = lib.mkOption {
               description = "the proxy it may use.";
               type = types.str;
             };
-            socks = types.mkOption {
+            socks = lib.mkOption {
               description = "the place to host the socks proxy.";
               type = types.str;
             };
-            listen = types.mkOption {
+            listen = = lib.mkOption {
               description = "the port to listen too.";
               type = types.str;
             };
-            connect = types.mkOption {
+            connect = lib.mkOption {
               description = "the connecting ip and port.";
               type = types.str;
             };
